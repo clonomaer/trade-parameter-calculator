53968a40456ecd55d867a7740c73e45de158f546
clonomaer <clonomaer@gmail.com>
2021-11-07 19:01:22 +0330

    mod/style/button active background changed

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -30,7 +30,7 @@ export default function Button({
             className={cn(
                 'rounded-lg border transition-colors duration-150 px-4 py-2 m-2 disabled:border-gray-500 disabled:bg-gray-800 disabled:text-gray-500 hover:border-gray-400',
                 active
-                    ? activeClassName ?? 'border-primary bg-gray-700'
+                    ? activeClassName ?? 'border-primary bg-primary-dark'
                     : inactiveClassName ?? 'border-primary-dark',
                 className,
             )}
