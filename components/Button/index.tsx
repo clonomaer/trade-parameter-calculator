fece89d94444507c8306c50d89c0c1239578546f
clonomaer <clonomaer@gmail.com>
2021-11-07 18:52:31 +0330

    add/component/Button

---
+++ components/Button/index.tsx
@@ -0,0 +1,41 @@
+import React, {
+    ButtonHTMLAttributes,
+    DetailedHTMLProps,
+    PropsWithChildren,
+} from 'react'
+import cn from 'classnames'
+import { ClassName } from 'types'
+
+export type ButtonProps = PropsWithChildren<{
+    className?: ClassName
+    activeClassName?: ClassName
+    inactiveClassName?: ClassName
+    active?: boolean
+}> &
+    DetailedHTMLProps<
+        ButtonHTMLAttributes<HTMLButtonElement>,
+        HTMLButtonElement
+    >
+
+export default function Button({
+    className,
+    children,
+    active,
+    activeClassName,
+    inactiveClassName,
+    ...props
+}: ButtonProps): React.ReactElement | null {
+    return (
+        <button
+            className={cn(
+                'rounded-lg border transition-colors duration-150 px-4 py-2 m-2 disabled:border-gray-500 disabled:bg-gray-800 disabled:text-gray-500 hover:border-gray-400',
+                active
+                    ? activeClassName ?? 'border-primary bg-gray-700'
+                    : inactiveClassName ?? 'border-primary-dark',
+                className,
+            )}
+            {...props}>
+            {children}
+        </button>
+    )
+}
