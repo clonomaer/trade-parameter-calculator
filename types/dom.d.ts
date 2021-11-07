beaf76bde803abf63ddede45171ec358515541b2
clonomaer <clonomaer@gmail.com>
2021-11-07 18:53:36 +0330

    fix/type/missing imageSrc type

--- types/dom.d.ts
+++ types/dom.d.ts
@@ -1,1 +1,5 @@
+import { ValueTypeOfKey } from 'types'
+import { ImageProps } from 'next/image'
+
 export type ClassName = string | { [className: string]: boolean } | ClassName[]
+export type ImageSrc = ValueTypeOfKey<ImageProps, 'src'>
