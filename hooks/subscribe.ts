349a58d1e1a3cb80df97d0b431eb435e45056e13
clonomaer <clonomaer@gmail.com>
2021-11-08 17:26:45 +0330

    add/useSubscribe/accept null|undefined

--- hooks/subscribe.ts
+++ hooks/subscribe.ts
@@ -5,25 +5,25 @@ import { LazyEval } from 'types'
 import { unLazy } from 'utils/un-lazy'
 
 export function useSubscribe<T>(
-    observable: LazyEval<Observable<T>>,
+    observable: LazyEval<Observable<T> | null | undefined>,
     observer?: Partial<Observer<T>>,
     dependencies?: DependencyList,
 ): void
 export function useSubscribe<T>(
-    observable: LazyEval<Observable<T>>,
+    observable: LazyEval<Observable<T> | null | undefined>,
     observer?: (value: T) => void,
     dependencies?: DependencyList,
 ): void
 
 export function useSubscribe<T>(
-    observable: LazyEval<Observable<T>>,
+    observable: LazyEval<Observable<T> | null | undefined>,
     observer?: any, // eslint-disable-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
     dependencies?: DependencyList,
 ): void {
     useEffect(() => {
-        const subscription = unLazy(observable).subscribe(observer)
+        const subscription = unLazy(observable)?.subscribe(observer)
         return () => {
-            subscription.unsubscribe()
+            subscription?.unsubscribe()
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [
