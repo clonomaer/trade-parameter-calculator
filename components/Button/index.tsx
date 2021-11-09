0a3a4344c736e060ca1cd22812889f350258d2c3
clonomaer <clonomaer@gmail.com>
2021-11-09 14:58:06 +0330

    add/style/button active style

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -30,8 +30,10 @@ export default function Button({
             className={cn(
                 'rounded-lg border transition-colors duration-150 px-4 py-2 m-2 disabled:border-gray-500 disabled:bg-gray-800 disabled:text-gray-500 hover:border-gray-400',
                 active
-                    ? activeClassName ?? 'border-primary bg-primary-dark'
-                    : inactiveClassName ?? 'border-primary-dark',
+                    ? activeClassName ??
+                          'border-primary bg-primary-dark active:bg-primary-dark'
+                    : inactiveClassName ??
+                          'border-primary-dark active:bg-primary-dark',
                 className,
             )}
             {...props}>
