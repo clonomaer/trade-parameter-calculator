ea07709234269572d1cdba551b4248b6c395ebd0
clonomaer <clonomaer@gmail.com>
2021-11-09 14:57:50 +0330

    add/NumberInput/general improvements for better usability

--- components/NumberInput/index.tsx
+++ components/NumberInput/index.tsx
@@ -2,20 +2,35 @@ import React from 'react'
 import { Input } from 'components/Input'
 import { NumbersValidatorOperator } from 'operators/numbers-validator'
 import { CurrencyFormatterOperator } from 'operators/currency-formatter'
+import cn from 'classnames'
+import styles from './styles.module.css'
 
 export type NumberInputProps = Omit<
     React.ComponentProps<typeof Input>,
     'validationFactory' | 'formatterFactory'
 >
 
-export default function NumberInput(
-    props: NumberInputProps,
-): React.ReactElement | null {
+export default function NumberInput({
+    className,
+    onWheel,
+    onFocus,
+    ...props
+}: NumberInputProps): React.ReactElement | null {
     return (
         <Input
+            {...props}
             validationFactory={NumbersValidatorOperator()}
             formatterFactory={CurrencyFormatterOperator()}
-            {...props}
+            type="number"
+            className={cn(styles.input, className)}
+            onWheel={e => {
+                onWheel?.(e)
+                ;(e.target as HTMLInputElement).blur()
+            }}
+            onFocus={e => {
+                onFocus?.(e)
+                ;(e.target as HTMLInputElement).select()
+            }}
         />
     )
 }
